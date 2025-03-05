a59b1bf4c95dcee82268067bd658bffa1208780f
johan <johanwp97@gmail.com>
2025-03-04 20:13:07 -0500

    game-complete-also-components-files-are-created-in-their-folder

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# React + Vite
+# Triki
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a little game that was develop with Vue+React. Target its very simple, align three times your object (X or O), objects have to be next to each other, it can be vertical, horizontal or diagonal.
 
 Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
 ## Expanding the ESLint configuration
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,11 @@ import { Children, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Square} from './Components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinner, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './Components/WinnerModal.jsx'
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-
-const Square = ({children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () =>{
-    updateBoard(index)
-  }
-
-  return(
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null)) 
@@ -41,19 +15,10 @@ function App() {
 
   const [winner, setWinner] = useState(null)
 
-  const checkWinner = (boardToCheck) => {
-    //check for every posible combos to win
-    for(const combo of WINNER_COMBOS){
-      const[a, b, c] = combo
-      if(
-        boardToCheck[a] && boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ){
-        return boardToCheck[a]
-      }
-    }
-    //if there is a draw
-    return null
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
   }
 
   const updateBoard = (index) =>{
@@ -71,22 +36,25 @@ function App() {
     const newWinner = checkWinner(newBoard)
     if (newWinner){
       setWinner(newWinner)
+    } else if (checkEndGame(newBoard)){
+      setWinner(false)
     }
   }
 
   return (
     <main className='board'>
-      <h1> "Juego de Triki" </h1>  
+      <h1> "Juego de Triki" </h1>
+      <button onClick={resetGame}> Reinciar Juego </button>  
       <section className='game'>
         {
-          board.map((_, index) =>{
+          board.map((square, index) =>{
             return (
               <Square
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
               >
-                {board[index]}
+                {square}
 
               </Square>
             )
@@ -99,6 +67,8 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
 
   </main>
   )
